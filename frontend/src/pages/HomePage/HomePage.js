28fa5c4e0270b342c3311effb8b9ced10f1cbf9a
Miller MA <this.is.millerma@gmail.com>
2022-03-25 15:41:35 -0600

    local version control

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -228,7 +228,7 @@ export default function HomePage() {
                         ))}
                     </TabPanel>
                     <TabPanel value="2" sx={{p:0}}>
-                        {inbox.filter(post => post.visibility !== "FRIENDS").map((post, index) => (
+                        {inbox.filter(post => post.visibility === "FRIENDS").map((post, index) => (
                             (<Grid item xs={12} key={index}> 
                                 <FeedCard allLikes= {allLikes} post={post} isOwner={post.author.id === author.url} fullWidth={true} alertError={alertError} alertSuccess={alertSuccess} updateFeed={updateFeed} removeFromFeed={removeFromFeed} /> 
                             </Grid>)
