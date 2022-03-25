917a6903f47f3f1313b7d7b04550721c615cda50
Miller MA <this.is.millerma@gmail.com>
2022-03-24 22:43:57 -0600

    Likes and liked test established

--- backend/comment/tests.py
+++ backend/comment/tests.py
@@ -27,35 +27,34 @@ def create_public_post(author):
             }
     return Post.objects.create(**data)
 
-# def create_post_comment(post):
+def create_post_comment(post):
 
-#     data = {"type": "comments",  
-#             "comment": "Post a comment", 
-#             "contentType": Comment.ContentType.PLAIN_TEXT, 
-#             "author_url": post.author.id,
-#             "post": post, 
-#     }
-
-#     return Comment.objects.create(**data)
-
+    data = {"type": "comments",  
+            "comment": "Post a comment", 
+            "contentType": Comment.ContentType.PLAIN_TEXT, 
+            "author_url": post.author.id,
+            "post": post, 
+    }
+    return Comment.objects.create(**data)
 
 
-class CommentTests(APITestCase):
 
-    def setUp(self) -> None:
-        self.user = create_user("SuperUser")
-        self.public_post = create_public_post(self.user.author)
-        print("self.public_post is: ", self.public_post)
-        # self.post_comment = create_post_comment(self.public_post)
-        # self.friend_post = create_friend_post(self.user.author)
+# class CommentTests(APITestCase):
 
-    # def test_get_comment(self):
-    #     """ Ensure we can create a new account object. """
-    #     # url = f"/api/authors/{self.user.author.local_id}/inbox/"
-    #     url = f"/api/authors/{self.public_post.author.id}posts/{self.public_post.id}/comments/"
-    #     self.client.force_authenticate(user=self.user)
-    #     response = self.client.get(url)
-    #     print(response.data)
-    #     print(self.post_comment)
+#     def setUp(self) -> None:
+#         self.user = create_user("SuperUser")
+#         self.public_post = create_public_post(self.user.author)
+#         print("self.public_post is: ", self.public_post.author.id)
+#         self.post_comment = create_post_comment(self.public_post)
+#         # self.friend_post = create_friend_post(self.user.author)
+
+#     def test_get_comment(self):
+#         """ Ensure we can create a new account object. """
+#         # url = f"/api/authors/{self.user.author.local_id}/inbox/"
+#         url = f"/api/authors/{self.public_post.author.id}posts/{self.public_post.id}/comments/"
+#         self.client.force_authenticate(user=self.user)
+#         response = self.client.get(url)
+#         print(response.data)
+#         print(self.post_comment)
 
 
