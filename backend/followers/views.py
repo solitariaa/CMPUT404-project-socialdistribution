7e861340f05e9d7cfd285c2678bca3c93da85628
JoshuaBillson <jmbillson@outlook.com>
2022-03-28 02:58:16 -0600

    Connection Leak In Followers/Following Views

--- backend/followers/views.py
+++ backend/followers/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from .serializers import FollowerSerializer, FollowingSerializer
 from rest_framework.response import Response
 from .models import Follower, Following
-from backend.helpers import get_author, get, extract_remote_id, extract_profile_image
+from backend.helpers import get_author, get, extract_remote_id, extract_profile_image, get_author_list, validate_author
 from authors.models import Author
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.authentication import BasicAuthentication, TokenAuthentication
@@ -49,9 +49,8 @@ def friends_list(request, author):
 @permission_classes([IsAuthenticated])
 def followers_list(request, author):
     author_object = get_object_or_404(Author, local_id=author)
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        future = executor.map(lambda x: get_author(x), [extract_remote_id(f.actor) for f in author_object.follower_set.all()])
-    followers = [f for f in future if "type" in f]
+    followers = get_author_list([extract_remote_id(f.actor) for f in author_object.follower_set.all()])
+    followers = [validate_author(f) for f in followers if "id" in f]
     followers.sort(key=lambda x: x["displayName"])
     return Response({"type": "followers", "items": followers}, content_type="application/json")
 
@@ -106,16 +105,16 @@ def following_detail(request, author, following):
 @authentication_classes([TokenAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def following_list(request, author):
+    # Get Following
     author_object = get_object_or_404(Author, local_id=author)
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        future_1 = executor.map(lambda x: get_author(x), [f.follows for f in author_object.following_set.all()])
-    following = [f for f in future_1 if "id" in f]
-    for author in following:
-        author["displayImage"] = extract_profile_image(author)
-        author["id"] = extract_remote_id(author["id"])
+    following = get_author_list([f.follows for f in author_object.following_set.all()])
+    following = [validate_author(f) for f in following if "id" in f]
 
+    # For Each Author We Are Following Check With Their Server If We Are Indeed A Follower
     with ThreadPoolExecutor(max_workers=1) as executor:
-        future_2 = executor.map(lambda x: get(x), [f"{f['id'].rstrip()}/followers/{author_object.id}/" for f in following])
-    confirmed_following = [f[0] for f in zip(following, [f for f in future_2]) if f[1] is not None and f[1].status_code != 404]
+        future = executor.map(lambda x: get(x), [f"{f['id'].rstrip('/')}/followers/{author_object.id.rstrip('/')}//" for f in following])
+
+    # Filter For Confirmed Followers
+    confirmed_following = [f[0] for f in zip(following, list(future)) if f[1] is not None and f[1].status_code != 404]
     confirmed_following.sort(key=lambda x: x["displayName"])
     return Response({"type": "following", "items": confirmed_following}, content_type="application/json")
